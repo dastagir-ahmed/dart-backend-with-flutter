6bbb4efbb1c95172a510a7c0ac2eafa7198d8979
Dylan <oncwnuD2vkq6iB13aUDw9ClTQjs0@git.weixin.qq.com>
2024-04-12 17:36:51 +0800

    Vania simple api

---
+++ android/app/src/main/kotlin/com/example/dart_backend_vania/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dart_backend_vania
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:dart_backend_vania/services/http_util.dart';
+import 'package:flutter/material.dart';
+
+import 'model/job_model.dart';
+
+Future<void> main() async {
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Dart backend'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  JobModel? job;
+
+  @override
+  void didChangeDependencies() {
+    myState();
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+
+  myState() async {
+    job = await HttpUtil().fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: job==null?Container():Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              job?.title ?? "Loading...", // Use null-ish coalescing operator
+            ),
+             const SizedBox(height: 5,),
+             Text(
+               job?.description ?? "Loading...", // Use null-ish coalescing operator
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
